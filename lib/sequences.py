2208f0b6d0ef673a791c78c3a61bb772ed1552d1
Ron Maina <ronmwangi00@gmail.com>
2023-08-31 11:05:46 +0300

    Refactored code

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,11 +1,6 @@
-#!/usr/bin/env python3
-import ipdb;
-
 def remove_duplicates(sequence):
     if type(sequence) == list:
         return list(set(sequence))
     else:
         return tuple(set(sequence))
 
-
-# remove_duplicates()
